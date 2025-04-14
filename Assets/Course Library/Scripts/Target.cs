61088e781816cb472e3dfbfeddac390016e97af6
Zafer İntepe <108065413+IntepeZafer@users.noreply.github.com>
2025-04-14 17:02:21 +0300

    commit

--- Assets/Course Library/Scripts/Target.cs
+++ Assets/Course Library/Scripts/Target.cs
@@ -3,11 +3,11 @@
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
-    public float minSpeed;
-    public float maxSpeed;
-    public float maxTorque;
-    public float xRange;
-    public float ySpawnPos;
+    public float minSpeed = 12f;
+    public float maxSpeed = 16f;
+    public float maxTorque = 10f;
+    public float xRange = 4f;
+    public float ySpawnPos = -6f;
 
     private void Start()
     {
@@ -16,16 +16,28 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque() , ForceMode.Impulse);
         transform.position = RandomSpawnPos();
     }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+
     Vector3 RandomForce() // Uygulanacak random başlangıç hızı fonksiyonu
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
+
     float RandomTorque() // Uygulanacak random dönüş hızı fonksiyonu
     {
         return Random.Range(-maxTorque, maxTorque);
     } 
-    Vector3 RandomSpawnPos()
+
+    Vector3 RandomSpawnPos() // Uygulanacak random başlangıç konum fonksiyonu
     {
-        return new Vector3(Random.Range(xRange, xRange), ySpawnPos);
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
